6407e0ae06309b114833fec02c9eac18b13b68b6
andrezasecon <andrezasecon@gmail.com>
2022-03-11 16:41:20 -0300

    Save, useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,8 +1,9 @@
 import { Movie } from 'assets/types/movie';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { BASE_URL } from 'utils/requests';
+import { validateEmail } from 'utils/validate';
 import './styles.css'
 
 type Props = {
@@ -11,22 +12,53 @@ type Props = {
 
 function FormCard( { movieId } : Props) {
 
-    const [movie, setMovie ] = useState<Movie>();
+  const navigate = useNavigate();
+
+  const [movie, setMovie ] = useState<Movie>();
 
     useEffect(() => {
         axios.get(`${BASE_URL}/movies/${movieId}`)
             .then(response => {
                 setMovie(response.data);
             });
-    }, [movieId]);
+    })
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+    event.preventDefault();
+
+    const email = (event.target as any).email.value;
+    const score = (event.target as any).score.value;
+
+    if(!validateEmail(email)){
+        return;
+    }
+
+    const config: AxiosRequestConfig = {
+      baseURL: BASE_URL,
+      method: 'PUT',
+      url: '/scores',
+      data: {
+        email: email,
+        movieId: movieId,
+        score: score
+      }
+    }
+
+    axios(config).then(resp => {
+      console.log(resp.data);
+        navigate("/");
+
+    });
+
+  }
 
   return (
     <div className="dsmovie-form-container">
       <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie?.title} />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie?.title}</h3>
-        <form className="dsmovie-form">
+        <form className="dsmovie-form" onSubmit={handleSubmit}>
           <div className="form-group dsmovie-form-group">
             <label htmlFor="email">Informe seu email</label>
             <input type="email" className="form-control" id="email" />
@@ -56,3 +88,7 @@ function FormCard( { movieId } : Props) {
 }
 
 export default FormCard;
+function userNavigate() {
+  throw new Error('Function not implemented.');
+}
+
